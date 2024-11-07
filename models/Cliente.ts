f8cad0ed64ce6c912a2468108f88390206f69650
dumenicolas45@gmail.com <nicolas@DESKTOP-DAMTQ93.>
2024-11-07 13:55:13 -0800

    actualizacion de cliente, veterinaria y proveedor

--- models/Cliente.ts
+++ models/Cliente.ts
@@ -1,51 +1,73 @@
+export class Cliente {
+  private id: number;
+  private nombre: string;
+  private direccion: string;
+  private telefono: number;
+  private esVip: boolean;
+  private cantVisitas: number;
 
-import { Veterinaria } from "./Veterinaria";
-export class Cliente extends Veterinaria {
-    protected telefono: number
-    protected esVip: boolean;
-    protected cantVisitas: number;
+  constructor(id: number, nombre: string, direccion: string, telefono: number, esVip: boolean = false, cantVisitas: number = 0) {
+    this.id = id;
+    this.nombre = nombre;
+    this.direccion = direccion;
+    this.telefono = telefono;
+    this.esVip = esVip;
+    this.cantVisitas = cantVisitas;
+  }
 
-    constructor(id: number, nombre: string, direccion: string, telefono: number, esVip: boolean, cantVisitas: number) {
-        super(id, nombre, direccion,telefono)
-        this.telefono = telefono;
-        this.esVip = esVip;
-        this.cantVisitas = cantVisitas
-    }
+  public getId(): number {
+    return this.id;
+  }
 
-    public getId(): number {
-        return this.id;
+  public setId(id: number): void {
+    this.id = id;
+  }
+
+  public getNombre(): string {
+    return this.nombre;
+  }
+
+  public setNombre(nombre: string): void {
+    this.nombre = nombre;
+  }
+
+  public getDireccion(): string {
+    return this.direccion;
+  }
+
+  public setDireccion(direccion: string): void {
+    this.direccion = direccion;
+  }
+
+  public getTelefono(): number {
+    return this.telefono;
+  }
+
+  public setTelefono(telefono: number): void {
+    this.telefono = telefono;
+  }
+
+  public getEsVip(): boolean {
+    return this.esVip;
+  }
+
+  public setEsVip(esVip: boolean): void {
+    this.esVip = esVip;
+  }
+
+  public getCantVisitas(): number {
+    return this.cantVisitas;
+  }
+
+  public setCantVisitas(cantVisitas: number): void {
+    this.cantVisitas = cantVisitas;
+  }
+
+
+  public registrarVisita(): void {
+    this.cantVisitas++;
+    if (this.cantVisitas >= 5) {
+      this.esVip = true;
     }
-    public setId(Id: number) {
-        this.id = Id;
-    }
-    getNombre(): string {
-        return this.nombre
-    }
-    setNombre(nombre: string): void {
-        this.nombre = nombre;
-    }
-    getDireccion(): string {
-        return this.direccion;
-    }
-    setDireccion(direccion: string): void {
-        this.direccion = direccion
-    }
-    getTelefono(): number {
-        return this.telefono
-    }
-    setTelefono(telefono: number): void {
-        this.telefono = telefono
-    }
-    getEsVip(): boolean {
-        return this.esVip
-    }
-    setEsVip(esVip: boolean): void {
-        this.esVip = esVip
-    }
-    getCantVisitas(): number {
-        return this.cantVisitas
-    }
-    setCantVisitas(cantVisitas: number): void {
-        this.cantVisitas = cantVisitas;
-    }
+  }
 }
