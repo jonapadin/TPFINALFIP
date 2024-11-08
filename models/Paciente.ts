129aa220bdc00e12d87a49b4fbeab2f1fc6819a2
Jonathan Padin <padinjonathan1@gmail.com>
2024-11-07 21:09:31 -0300

    Actualizado

--- models/Paciente.ts
+++ models/Paciente.ts
@@ -1,13 +1,11 @@
-import {Cliente} from "./Cliente"
+import {Cliente} from "./Cliente";
 
 export class Paciente extends Cliente {
-    protected id: number;
-    protected nombre: string;
     protected especie : string;
     protected idDuenio: number;
 
     constructor(id:number, nombre:string,especie:string,idDuenio:number){
-        super(id,nombre,telefono,esVip,cantVisitas)
+        super(id,nombre,direccion,telefono,esVip,cantVisitas)
         this.especie = especie;
         this.idDuenio = idDuenio;
     }
