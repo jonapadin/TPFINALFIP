b6125854ad94e46b2b321522271f7bfe19da444e
dumenicolas45@gmail.com <nicolas@DESKTOP-DAMTQ93.>
2024-11-14 13:54:52 -0800

    rework

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,50 +1,11 @@
-const form = document.getElementById("formInicio") as HTMLFormElement;
-const usuario = document.getElementById("usuario") as HTMLInputElement;
-const pass = document.getElementById("pass") as HTMLInputElement;
-const btn = document.getElementById("btn") as HTMLButtonElement ;
-const errorU = document.getElementById("errorUsuario") as HTMLSpanElement;
-const errorP = document.getElementById("errorPass") as HTMLSpanElement;
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Hello world");
 
-function validarForm(): boolean {
-    let esValido = true;
-    if(!usuario.value.trim()){
-         errorU.classList.remove("ocultar");
-         esValido = false;
-    } else {
-        errorU.classList.add("ocultar");
-    }
+  const nombreVet = document.getElementById("veterinaria");
+  const direccionVet = document.getElementById("direccionVet");
 
-    if(!pass.value.trim()){
-        errorP.classList.remove("ocultar");
-        esValido = false;
-   } else {
-       errorP.classList.add("ocultar");
-   }
+  console.log(nombreVet, direccionVet);
 
-   return esValido
-}
 
-form?.addEventListener("submit", function(event){
-    event.preventDefault();
 
-    if(!validarForm){
-        return
-    }
-
-    const formData = new FormData(form as HTMLFormElement, btn as HTMLElement);
-    console.log(formData);
-
-    const url = "../Utils/usuarios.json"
-
-    fetch(url)
-    .then(resp => resp.json())
-    .then(data =>{
-        if(data.success){
-            window.location.href="dashboard.html"
-        }else{
-            alert("Usuario o contraseña incorrectos")
-        }
-        console.log(data);    
-    })
-    .catch(err => console.error("error", err));
 })
