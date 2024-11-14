a7688ff1ed0e41aa54a21c9cb6416f656facb8de
estebanhermada <estebanhermada@gmail.com>
2024-11-13 21:57:40 -0300

    creacion de index y tareas

--- index.ts
+++ index.ts
@@ -3,28 +3,17 @@ import { Paciente } from "./models/Paciente";
 import { Proveedor } from "./models/Proveedor";
 import { Veterinaria } from "./models/Veterinaria";
 
+import * as rls from "readline-sync";
 
-let veterinaria1 = new Veterinaria("scooby", "Av libertador 2323");
+let opcion1: number;
+let opcion2: number;
+let opcion3: number;
+let opcion4: number;
 
-let proveedor1 = new Proveedor("Pedro", 665298);
-
-let cliente1 = new Cliente("Omar", "AvBaltica 2323,54654", 3)
-
-let paciente1 = new Paciente("Firulai", "anfiel2334", 545646, true, 6, "Perro", cliente1.getId())
-
-veterinaria1.agregarCliente(cliente1);
-veterinaria1.agregarProveedor(proveedor1);
-veterinaria1.agregarPaciente(paciente1);
-
-console.log(veterinaria1);
-
-veterinaria1.eliminarCliente(1);
-
-console.log(veterinaria1);
-
-veterinaria1.modificarProveedor(proveedor1.getId(), "pepe", 23232323)
-
-console.log(veterinaria1);
+function mostrarMenu() {
+    const id=
+}
 
 
+//= rls.question("Introduzca su nombre: ");
 
