e1f8fc9e75cd8d832d208469f35820947e58a613
estebanhermada <estebanhermada@gmail.com>
2024-11-15 15:41:58 -0300

    agregar elementos a funcion modal

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,44 @@
 
 import { Veterinaria } from '../models/veterinaria.ts'
 document.addEventListener("DOMContentLoaded", () => {
-
+  
+  //menu desplegable
   const mostrarMenu1 = document.getElementById("mostrarMenu1");
   const mostrarMenu2 = document.getElementById("mostrarMenu2");
-
-  const resultadoDiv = document.getElementById("resultado");
   const desplegable1 = document.getElementById("desplegable1");
   const desplegable2 = document.getElementById("desplegable2");
 
+  //panel de agregar
+  const resultadoDiv = document.getElementById("resultado");
+  function mostrarPanel(){
+    const contenedor = document.createElement("section");
+    const div = document.createElement("div");
+    const divContenedorDireccion = document.createElement("div");
+    const labelDireccion = document.createElement("label");
+    labelDireccion.textContent = "Direccion:"
+    const inputDireccion = document.createElement("input");
+    inputDireccion.type = "text"
+    inputDireccion.id = "inputDireccion"
+    inputDireccion.placeholder = "Ej. Calle 1234"
+
+    contenedor.appendChild(div);
+    div.appendChild(divContenedorDireccion);
+    divContenedorDireccion.appendChild(labelDireccion);
+    divContenedorDireccion.appendChild(inputDireccion);
+
+
+    const div2 = document.createElement("div");
+    const divContenedorNombre = document.createElement("div");
+    const labelNombre = document.createElement("label");
+    labelDireccion.textContent = "Direccion:"
+    const inputNombre = document.createElement("input");
+
+    contenedor.appendChild(div2);
+    div2.appendChild(divContenedorNombre);
+    divContenedorNombre.appendChild(labelNombre);
+    divContenedorNombre.appendChild(inputNombre);
+
+  } 
 
   // Crear instancia de Veterinaria
   const veterinaria1 = new Veterinaria();
@@ -56,6 +86,22 @@ document.addEventListener("DOMContentLoaded", () => {
     return btn;
   }
 
+  function crearInput(label = "", id = "", clase = '') {
+
+    const btn = document.createElement("button");
+
+    // Asignar el id al botón
+    btn.id = id;
+
+    btn.classList.add(clase);
+
+    // Asignar el texto del botón
+    btn.textContent = label;
+
+
+    // Devolver el botón creado
+    return btn;
+  }
 
   const btnMostrarVet = crearBoton("Mostrar Lista", "Mostrar", "btn-mostrar");
   const btnAgregarVet = crearBoton("Agregar", "Agregar", "btn-primario");
